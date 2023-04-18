eb5ecd95908c24b281740e1c57eb60e6991539a3
Yash <imyashpawar@gmail.com>
2023-04-18 16:26:15 +0530

    Added:Float Max

--- src/com/birdgelabz/MaxVar.java
+++ src/com/birdgelabz/MaxVar.java
@@ -8,7 +8,7 @@ public class MaxVar
 
 
 
-    public Integer findmax(Integer num1, Integer num2, Integer num3) {
+    public Integer findMaxInt(Integer num1, Integer num2, Integer num3) {
 
         Integer max = num1;
         if (num2.compareTo(max) > 0) {
@@ -21,4 +21,19 @@ public class MaxVar
         return max;
 
     }
+
+    public Float findMaxFloat(Float num1, Float num2, Float num3) {
+
+        Float max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        System.out.println(max);
+        return max;
+
+    }
+
 }
--- src/com/birdgelabz/MaxVarMain.java
+++ src/com/birdgelabz/MaxVarMain.java
@@ -6,8 +6,10 @@ public class MaxVarMain {
     {
 
         MaxVar maxVar = new MaxVar();
-        maxVar.findmax(100,15,16);
-        maxVar.findmax(15,200,50);
+        maxVar.findMaxInt(100,15,16);
+        maxVar.findMaxInt(15,200,50);
+
+        maxVar.findMaxFloat(10.30f, 2.4f,6.4f);
 
 
     }
