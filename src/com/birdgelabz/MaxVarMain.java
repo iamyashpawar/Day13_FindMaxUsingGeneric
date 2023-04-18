559bb88f3ce68e7441d2c98431cf85930fce1079
Yash <imyashpawar@gmail.com>
2023-04-18 15:59:07 +0530

    Added: Max Varible

--- src/com/birdgelabz/MaxVarMain.java
+++ src/com/birdgelabz/MaxVarMain.java
@@ -5,6 +5,10 @@ public class MaxVarMain {
     public static void main(String[] args)
     {
 
+        MaxVar maxVar = new MaxVar();
+        maxVar.findmax(100,15,16);
+        maxVar.findmax(15,200,50);
+
 
     }
 
